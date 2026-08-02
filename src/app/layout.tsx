33758da98705c4adb441c6154c10d0aee35c5f2c
Kavindu <kavinduuni@gmail.com>
2026-08-02 13:58:55 +0530

    chore: apply lightweight lusion aesthetic updates and install Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { SiteHeader } from "@/components/SiteHeader";
 import { SiteFooter } from "@/components/SiteFooter";
 import { MobileCta } from "@/components/MobileCta";
 import { MotionController } from "@/components/MotionController";
-import "@fontsource-variable/open-sans/wght.css";
+import "@fontsource-variable/inter/wght.css";
 import "@fontsource-variable/playfair-display/wght.css";
 import "../../public/assets/css/styles.css";
 
